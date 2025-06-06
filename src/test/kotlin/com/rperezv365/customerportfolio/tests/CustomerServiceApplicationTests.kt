f221769be97211ff320dc63c1aad937b06e53309
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-06-06 16:24:52 +0200

    167. Integration Tests - Part 2b

--- src/test/kotlin/com/rperezv365/customerportfolio/tests/CustomerServiceApplicationTests.kt
+++ src/test/kotlin/com/rperezv365/customerportfolio/tests/CustomerServiceApplicationTests.kt
@@ -22,20 +22,19 @@ class CustomerServiceApplicationTests {
 
     @Test
     fun buyAndShell() {
-
-        /*
+        val customerId = "6842f8cb7f09348aa874ee91"
         // buy
         val buyRequest1 = StockTradeRequest(Ticker.GOOGLE, 100, 5, TradeAction.BUY)
-        trade("6841f71deb46c6922cf93e14", buyRequest1, HttpStatus.OK)
+        trade(customerId, buyRequest1, HttpStatus.OK)
             .jsonPath("$.balance").isEqualTo(9500)
             .jsonPath("$.totalPrice").isEqualTo(500)
 
         val buyRequest2 = StockTradeRequest(Ticker.GOOGLE, 100, 10, TradeAction.BUY)
-        trade("6841f71deb46c6922cf93e14", buyRequest2, HttpStatus.OK)
+        trade(customerId, buyRequest2, HttpStatus.OK)
             .jsonPath("$.balance").isEqualTo(8500)
             .jsonPath("$.totalPrice").isEqualTo(1000)
 
-        getCustomer("6841f71deb46c6922cf93e14", HttpStatus.OK)
+        getCustomer(customerId, HttpStatus.OK)
             .jsonPath("$.holdings").isNotEmpty
             .jsonPath("$.holdings.length()").isEqualTo(1)
             .jsonPath("$.holdings[0].ticker").isEqualTo("GOOGLE")
@@ -43,17 +42,16 @@ class CustomerServiceApplicationTests {
 
         // sell
         val sellRequest1 = StockTradeRequest(Ticker.GOOGLE, 110, 5, TradeAction.SELL)
-        trade("6841f71deb46c6922cf93e14", sellRequest1, HttpStatus.OK)
+        trade(customerId, sellRequest1, HttpStatus.OK)
             .jsonPath("$.balance").isEqualTo(9050)
             .jsonPath("$.totalPrice").isEqualTo(550)
-        */
 
         val sellRequest2 = StockTradeRequest(Ticker.GOOGLE, 110, 10, TradeAction.SELL)
-        trade("6841f71deb46c6922cf93e14", sellRequest2, HttpStatus.OK)
+        trade(customerId, sellRequest2, HttpStatus.OK)
             .jsonPath("$.balance").isEqualTo(10150)
             .jsonPath("$.totalPrice").isEqualTo(1100)
 
-        getCustomer("6841f71deb46c6922cf93e14", HttpStatus.OK)
+        getCustomer(customerId, HttpStatus.OK)
             .jsonPath("$.holdings").isNotEmpty
             .jsonPath("$.holdings.length()").isEqualTo(1)
             .jsonPath("$.holdings[0].ticker").isEqualTo("GOOGLE")
@@ -61,9 +59,36 @@ class CustomerServiceApplicationTests {
     }
 
     @Test
+    fun customerNotFound() {
+        val customerId = "6842f8cb7f09348aa874ee94"
+        getCustomer(customerId, HttpStatus.NOT_FOUND)
+            .jsonPath("$.detail").isEqualTo("Customer [id=$customerId] is not found")
+
+        val sellRequest = StockTradeRequest(Ticker.GOOGLE, 110, 5, TradeAction.SELL)
+        trade(customerId, sellRequest, HttpStatus.NOT_FOUND)
+            .jsonPath("$.detail").isEqualTo("Customer [id=$customerId] is not found")
+    }
+
+    @Test
+    fun insufficientBalance() {
+        val customerId = "6842f8cb7f09348aa874ee93"
+        val buyRequest = StockTradeRequest(Ticker.GOOGLE, 100, 101, TradeAction.BUY)
+        trade(customerId, buyRequest, HttpStatus.BAD_REQUEST)
+            .jsonPath("$.detail").isEqualTo("Customer [id=$customerId] does not have enough funds to complete the transaction")
+    }
+
+    @Test
+    fun insufficientShares() {
+        val customerId = "6842f8cb7f09348aa874ee93"
+        val sellRequest = StockTradeRequest(Ticker.GOOGLE, 100, 1, TradeAction.SELL)
+        trade(customerId, sellRequest, HttpStatus.BAD_REQUEST)
+            .jsonPath("$.detail").isEqualTo("Customer [id=$customerId] does not have enough shares to complete the transaction")
+    }
+
+    @Test
     fun customerInformation() {
-        getCustomer("683b03f901873864a5f80d79", HttpStatus.OK)
-            .jsonPath("$.name").isEqualTo("Sam")
+        getCustomer("6842f8cb7f09348aa874ee93", HttpStatus.OK)
+            .jsonPath("$.name").isEqualTo("John")
             .jsonPath("$.balance").isEqualTo(10000)
             .jsonPath("$.holdings").isEmpty
     }
@@ -71,7 +96,7 @@ class CustomerServiceApplicationTests {
     fun getCustomer(customerId: String, expectedStatus: HttpStatus): WebTestClient.BodyContentSpec {
 
         return client.get()
-            .uri("/api/v1/customers/{cusotmerId}", customerId)
+            .uri("/api/v1/customers/{customerId}", customerId)
             .exchange()
             .expectStatus().isEqualTo(expectedStatus)
             .expectBody()
