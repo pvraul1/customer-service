b725c816b73b1858361d52d313ce26410064e2f5
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-06-19 12:05:22 +0200

    187. Trading Platform - Final Demo

--- src/main/kotlin/com/rperezv365/customerportfolio/controller/CustomerController.kt
+++ src/main/kotlin/com/rperezv365/customerportfolio/controller/CustomerController.kt
@@ -5,15 +5,18 @@ import com.rperezv365.customerportfolio.dto.StockTradeRequest
 import com.rperezv365.customerportfolio.dto.StockTradeResponse
 import com.rperezv365.customerportfolio.service.CustomerService
 import com.rperezv365.customerportfolio.service.TradeService
+import mu.KLogging
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/api/v1/customers")
+@RequestMapping("/customers")
 class CustomerController(
     private val customerService: CustomerService,
     private val tradeService: TradeService
 ) {
+
+    companion object : KLogging()
 
     @GetMapping("/{customerId}")
     fun getCustomerInformation(@PathVariable customerId: String): Mono<CustomerInformation> {
@@ -22,6 +25,9 @@ class CustomerController(
 
     @PostMapping("/{customerId}/trade")
     fun trade(@PathVariable customerId: String, @RequestBody mono: Mono<StockTradeRequest>) : Mono<StockTradeResponse> {
+
+        logger.info { "trade service (in customer-service controller) was called!" }
+
         return mono.flatMap { request -> tradeService.trade(customerId, request) }
     }
 
